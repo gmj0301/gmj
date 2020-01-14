6e3c219f8fed0e9d168a0b89f9832e344275d55e
gmj0301 <59163102+gmj0301@users.noreply.github.com>
2020-01-14 20:24:07 +0800

    Update create_sudo.h

--- create_sudo.h
+++ create_sudo.h
@@ -1,21 +1,23 @@
+#pragma once
 #define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
-#include <time.h>
+#include <string.h>
 
 int shift[8] = { 3, 6, 1, 4, 7, 2, 5, 8 };
+int c1[3] = { 3, 4, 5 };
+int c2[3] = { 6, 7, 8 };
+int snum1 = 1, snum2 = 1;
 
-void create(int sudo[9][9]) {
-	for (int i = 1; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
+void produce(int sudo[9][9])  //第一行已知，通过变换得到完整的数独终局
+{
+	for (int i = 1; i < 9; i++)
+		for (int j = 0; j < 9; j++)
 			sudo[i][j] = sudo[0][(j + shift[i - 1]) % 9];
-			//第i行第j个是第一行左移shift[i]得到的
-			//相当于 sudo[i][j] = sudo[0][j右移shift[i]]
-			//j右移后可能超出9，所以要模9取余
-		}
-	}
+	//第i行是第一行左移shift[i]得到的  即sudo[i][j] = sudo[0][j右移shift[i]]
+	//j右移后可能超出9，所以要模9取余
 }
 
-void swap(int& a, int& b)
+void swap(int& a, int& b) //交换函数，交换a和b
 {
 	int temp = a;
 	a = b;
@@ -45,10 +47,13 @@ bool nextPermutation(int* p, int n)
 	//倒置p[last]到p[i]   
 	for (j = last, k = i; j > k; j--, k++)
 		swap(p[j], p[k]);
+
 	return true;
 }
 
-void jiaohan_1(int s1,int *c1) {
+void jiaohan_1(int s1, int* c1)     //中间4，5，6行的变换
+{
+	if (s1 == 1) { c1[0] = 3; c1[1] = 4; c1[2] = 5; }
 	if (s1 == 2) { c1[0] = 3; c1[1] = 5; c1[2] = 4; }
 	if (s1 == 3) { c1[0] = 4; c1[1] = 3; c1[2] = 5; }
 	if (s1 == 4) { c1[0] = 4; c1[1] = 5; c1[2] = 3; }
@@ -56,7 +61,9 @@ void jiaohan_1(int s1,int *c1) {
 	if (s1 == 6) { c1[0] = 5; c1[1] = 4; c1[2] = 3; }
 }
 
-void jiaohan_2(int s2, int *c2) {
+void jiaohan_2(int s2, int* c2)     //中间7，8，9行的变换
+{
+	if (s2 == 1) { c2[0] = 6; c2[1] = 7; c2[2] = 8; }
 	if (s2 == 2) { c2[0] = 6; c2[1] = 8; c2[2] = 7; }
 	if (s2 == 3) { c2[0] = 7; c2[1] = 6; c2[2] = 8; }
 	if (s2 == 4) { c2[0] = 7; c2[1] = 8; c2[2] = 6; }
@@ -64,55 +71,72 @@ void jiaohan_2(int s2, int *c2) {
 	if (s2 == 6) { c2[0] = 8; c2[1] = 7; c2[2] = 6; }
 }
 
-
-void c() {
-
-	FILE* fp = fopen("sudoku.txt", "w");/*以写模式("w")打开文件user.txt,如果不存在,会自动创建*/
-	int start = clock();
-	//fputs(pas, fp);/*将名称和密码以字符串形式写入文件*/
+void init(int sudo[9][9], int p[8]) //初始化，规定左上角第一个为1，第一行为123456789
+{
 	sudo[0][0] = 1;
 	for (int i = 1; i < 9; i++)
 		sudo[0][i] = i + 1;
-	p = new int[8];
 	for (int i = 0; i < 8; i++)
 		p[i] = i + 2;
-	for (int i = 0; i < N; i++) {
-		if (i > 0) fprintf(fp, "\n");
+}
 
-		if (snum1 == 1 && snum2 == 1) {
-			if (i > 0) {
+void output(int sudo[9][9], FILE* fp)  //输出函数
+{
+	jiaohan_1(snum1, c1);
+	jiaohan_2(snum2, c2); //判断当前两个参数表示哪两种顺序
+	//输出数独
+	for (int i = 0; i < 9; i++)
+	{
+		int k = i;
+		if (k >= 3 && k <= 5) k = c1[k - 3]; //如果当前是中间三行，按照顺序输出
+		else if (k > 5) k = c2[k - 6];       //如果当前是中间三行，按照顺序输出
+
+		for (int j = 0; j <= 8; j++) {
+			char x = sudo[k][j] + '0'; //转换成字符型
+			fputc(x, fp);         //写入文件
+			if (j != 8) fprintf(fp, " ");
+		}
+		fprintf(fp, "\n");
+	}
+}
+
+void change()
+{
+	if (snum2 == 6) {
+		snum2 = 1;
+		if (snum1 == 6) snum1 = 1;
+		else snum1++;
+	}
+	else snum2++;
+}
+
+void create(int N)
+{
+	int p[8], sudo[9][9];
+
+	FILE* fp = fopen("sudoku.txt", "w");
+	/*以写模式("w")打开文件user.txt,如果不存在,会自动创建*/
+
+	init(sudo, p); //初始化，自己设置第一个数独
+
+	for (int i = 0; i < N; i++)
+	{
+		if (i > 0) fprintf(fp, "\n");    //已经输完第一个数独，需要空一行
+
+		if (snum1 == 1 && snum2 == 1)    //之前数独所有行变换已经完成
+		{
+			if (i > 0) //i=0说明这是初始化的数独，不需要经过全排序
+			{
 				nextPermutation(p, 8);
 				for (int j = 1; j < 9; j++)
 					sudo[0][j] = p[j - 1];
 			}
-			create(sudo);
-		}
-		jiaohan_1(snum1,c1);
-		jiaohan_2(snum2,c2); //判断当前两个参数表示哪两种顺序
-		//输出数独
-		for (int i = 0; i < 9; i++)
-		{
-			int k = i;
-			if (k >= 3 && k <= 5) k = c1[k - 3]; //如果当前是中间三行，按照顺序输出
-			else if (k > 5) k = c2[k - 6];       //如果当前是中间三行，按照顺序输出
-
-			for (int j = 0; j <= 8; j++) {
-				x = sudo[k][j] + '0'; //转换成字符型
-				fputc(x, fp);         //写入文件
-				if(j != 8) fprintf(fp, " ");
-			}
-			fprintf(fp, "\n");
+			produce(sudo); //由第一行生成完整的数独
 		}
 
-		if (snum2 == 6) {
-			snum2 = 1;
-			if (snum1 == 6) snum1 = 1;
-			else snum1++;
-		}
-		else snum2++;
+		output(sudo, fp);
+
+		change();
 	}
 	fclose(fp);/*关闭文件*/
-	int end = clock();
-	printf("消耗的时间为：%dms\n", end - start);
-	return 0;
 }
