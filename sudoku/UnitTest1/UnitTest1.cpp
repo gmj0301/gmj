0b370ff84dfd14e808d1e462c2670004d34f2c96
gmj0301 <59163102+gmj0301@users.noreply.github.com>
2020-01-19 23:58:08 +0800

    Add files via upload

--- sudoku/UnitTest1/UnitTest1.cpp
+++ sudoku/UnitTest1/UnitTest1.cpp
@@ -56,7 +56,7 @@ namespace UnitTest1
 		TEST_METHOD(TestMethod_jiaohan_1)  //测create.cpp中的jiaohan_1函数
 		{
 			jiaohan_1(1, c1);
-			Assert::AreEqual(c1[0] == 3, true);
+			Assert::AreEqual(c1[0] == 3 && c1[1] == 4 && c1[2] == 5, true);
 			jiaohan_1(2, c1);
 			Assert::AreEqual(c1[2] == 4, true);
 			jiaohan_1(3, c1);
@@ -68,7 +68,6 @@ namespace UnitTest1
 			jiaohan_1(6, c1);
 			Assert::AreEqual(c1[1] == 4, true);
 		}
-
 		TEST_METHOD(TestMethod_jiaohan_2)  //测create.cpp中的jiaohan_2函数
 		{
 			jiaohan_2(1, c2);
@@ -99,6 +98,26 @@ namespace UnitTest1
 		}
 		TEST_METHOD(TestMethod_change)   //测create.cpp中的change函数
 		{
+			snum1 = 4; snum2 = 1;
+			change();
+			Assert::AreEqual(snum1 == 4, true);
+			Assert::AreEqual(snum2 == 2, true);
+			snum1 = 4; snum2 = 2;
+			change();
+			Assert::AreEqual(snum1 == 4, true);
+			Assert::AreEqual(snum2 == 3, true);
+			snum1 = 4; snum2 = 3;
+			change();
+			Assert::AreEqual(snum1 == 4, true);
+			Assert::AreEqual(snum2 == 4, true);
+			snum1 = 4; snum2 = 4;
+			change();
+			Assert::AreEqual(snum1 == 4, true);
+			Assert::AreEqual(snum2 == 5, true);
+			snum1 = 4; snum2 = 5;
+			change();
+			Assert::AreEqual(snum1 == 4, true);
+			Assert::AreEqual(snum2 == 6, true);
 			snum1 = 4; snum2 = 6;
 			change();
 			Assert::AreEqual(snum1 == 5, true);
@@ -107,10 +126,10 @@ namespace UnitTest1
 			change();
 			Assert::AreEqual(snum1 == 1, true);
 			Assert::AreEqual(snum2 == 1, true);
-			snum1 = 4; snum2 = 2;
+			snum1 = 1; snum2 = 1;
 			change();
-			Assert::AreEqual(snum1 == 4, true);
-			Assert::AreEqual(snum2 == 3, true);
+			Assert::AreEqual(snum1 == 1, true);
+			Assert::AreEqual(snum2 == 2, true);
 		}
 		TEST_METHOD(TestMethod_judge)  //测solve.cpp中的judge函数
 		{
@@ -120,6 +139,52 @@ namespace UnitTest1
 			Assert::AreEqual(judge(0, 2), false);
 			sudo[0][0] = 1; sudo[2][0] = 1;
 			Assert::AreEqual(judge(2, 0), false);
+		}
+        TEST_METHOD(TestMethod_serch) //测solve.cpp中的serch函数
+		{
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			for (int i = 0; i < 9; i++)
+				sudo[0][i] = 0;
+			for (int i = 0; i < 9; i++)
+				for (int j = 0; j < 9; j++)
+					if (sudo[i][j] == 0)
+						numl[j]++;
+			int h = serch();
+			Assert::AreEqual(h == 0, true);
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
+		}
+		TEST_METHOD(TestMethod_serch2) //测solve.cpp中的serch函数
+		{
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			for (int i = 0; i < 9; i++)
+				for (int j = 0; j < 9; j++)
+					if (sudo[i][j] == 0)
+						numl[j]++;
+			int h = serch();
+			Assert::AreEqual(h == -1, true);
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
+		}
+
+		TEST_METHOD(TestMethod_serch3) //测solve.cpp中的serch函数
+		{
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			sudo[0][0] = 0;
+			for (int i = 0; i < 9; i++)
+				for (int j = 0; j < 9; j++)
+					if (sudo[i][j] == 0)
+						numl[j]++;
+			int h = serch();
+			Assert::AreEqual(h == 0, true);
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
 		}
 		TEST_METHOD(TestMethod_solve_sudo) //测solve.cpp中的solve_sudo函数
 		{
@@ -137,7 +202,78 @@ namespace UnitTest1
 			Assert::AreEqual(sudo[0][1], 2);
 			Assert::AreEqual(sudo[0][2], 3);
 			Assert::AreEqual(sudo[0][3], 4);
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
 		}
 
+		TEST_METHOD(TestMethod_mmin) //测solve.cpp中的mmin函数
+		{
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			for (int i = 0; i < 9; i++)
+				sudo[0][i] = 0;
+			int k = mmin(0);
+			Assert::AreEqual(k == 0, true);
+		}
+
+		TEST_METHOD(TestMethod_write) //测write函数
+		{
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			judge_all = 0;
+			num_1 = 0;
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
+			write();
+			FILE* fp = fopen("solve_sudo.txt", "r");
+			char c = fgetc(fp);
+			int k = c - '0';
+			Assert::AreEqual(k == 1, true);
+			fclose(fp);
+		}
+
+		TEST_METHOD(TestMethod_create) //测create函数
+		{
+			create(1);
+			FILE* fp = fopen("sudoku.txt", "r");
+			char c = fgetc(fp);
+			int k = c - '0';
+			Assert::AreEqual(k == 1, true);
+			fclose(fp);
+
+		}
+		
+		TEST_METHOD(TestMethod_output)//测output函数
+		{
+			FILE* fp = fopen("st.txt","w");
+			int p[8];
+			init(sudo, p);
+			produce(sudo);
+			flag_first = 0;
+			output(sudo, fp);
+			fclose(fp);
+			fp = fopen("st.txt", "r");
+			char c[20];
+			fgets(c,20,fp);
+			Assert::AreEqual(c[0] == '1', true);
+			fclose(fp);
+		}
+
+		TEST_METHOD(TestMethod_solve)//测solve函数
+		{
+			char s[] = "wert.txt";
+			judge_all = 0;
+			num_1 = 0;
+			for (int i = 0; i < 9; i++)
+				numl[i] = 0;
+			solve(s);
+			FILE* fp = fopen("solve_sudo.txt", "a+");
+			char c[20];
+			fgets(c, 20, fp);
+			Assert::AreEqual(c[0] == '7', true);
+			fclose(fp);
+		}
 	};
 }
